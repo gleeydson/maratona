94f524f600916b6038921381151b713077082c75
Gleydson <gleydson.silvasousa@gmail.com>
2015-11-20 14:05:02 -0300

    pagina de cadastro de dificuldades

--- projeto/nav/qualquernome.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Gleydson
- * Date: 20/11/2015
- * Time: 12:32
- */
---
+++ projeto/qualquernome.php
@@ -0,0 +1,12 @@
+<?php
+
+
+require "funcoes.php";
+
+excluirDificuldade('4');
+
+
+/*if(salvarDificuldade('','desc','10','greeen')){
+    echo "inserido com sucessso";
+}else
+    echo "erro na insersao";*/
